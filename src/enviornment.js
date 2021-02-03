122173e091ca1149d741d95d78b93960130600a9
Pooja Bongirwar <pooja.bongirwar@deserve.com>
2021-02-03 19:12:53 +0530

    Replace \n in the env var with real line breaks

--- src/enviornment.js
+++ src/enviornment.js
@@ -1,6 +1,6 @@
 const Environment = {
   clientId: process.env.CLIENT_ID,
-  accessKey: process.env.access_key,
+  accessKey: process.env.access_key.replace(/\\n/gm, '\n'),
   keyId: process.env.key_id,
   redirectUri: process.env.redirect_uri,
   port: process.env.PORT
